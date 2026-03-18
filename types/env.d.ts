f0882bc6ce254d07f7a0d1886f8068f5228b4ef3
Kvaaak <veikkaknuutila@gmail.com>
2026-03-18 13:16:51 +0200

    eas.json

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,9 +1,3 @@
-// Source - https://stackoverflow.com/a/74246977
-// Posted by GollyJer
-// Retrieved 2026-03-12, License - CC BY-SA 4.0
-
 declare module '@env' {
   export const API_KEY: string;
-
-  // other ones
 }
