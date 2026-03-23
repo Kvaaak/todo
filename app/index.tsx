b95f73e193df4b3532b727cc96c900ae20a79fbc
Kvaaak <veikkaknuutila@gmail.com>
2026-03-23 11:08:32 +0200

    Changed modal.tsx to index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Pressable, StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
-export default function ModalScreen() {
+export default function MainScreen() {
   return (
     <ThemedView style={styles.container}>
 
